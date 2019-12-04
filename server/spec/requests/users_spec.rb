6a32b5285043f5e554bc5f6fedc27722af75d3ce
25 macamp <rateliasudaisuki@gmail.com>
2019-12-04 21:08:20 +0900

    テスト追加 when valid params #3

--- server/spec/requests/users_spec.rb
+++ server/spec/requests/users_spec.rb
@@ -2,17 +2,16 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   describe "POST /users" do
-    let(:invalid_params) do
-      {
-        user: {
-          name: "",
-          email: "",
-          password: ""
-        }
-      }
-    end
-
     context "when params invalid" do
+      let(:invalid_params) do
+        {
+          user: {
+            name: "",
+            email: "",
+            password: ""
+          }
+        }
+      end
       subject { post api_users_path, params: invalid_params }
 
       it "should return 422 status code" do
@@ -40,6 +39,36 @@ RSpec.describe "Users", type: :request do
         subject
 
         expect(json).to eq errors
+      end
+    end
+
+    context "when valid params" do
+      let(:user) { build :user }
+      let(:valid_params) do
+        {
+          user: {
+            name: user.name,
+            email: user.email,
+            password: user.password
+          }
+        }
+      end
+
+      subject { post api_users_path, params: valid_params }
+
+      it "should return 201 status code" do
+        subject
+
+        expect(response).to have_http_status :created
+      end
+
+      it "should return json body" do
+        subject
+
+        expect(json).to include({
+          "name" => user.name,
+          "email" => user.email
+        })
       end
     end
   end
