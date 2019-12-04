5acec3cfe854b6a9391d75db8bee834d1d9d1694
25 macamp <rateliasudaisuki@gmail.com>
2019-12-04 16:32:32 +0900

    api_users_pathを追加 #3

--- server/spec/requests/users_spec.rb
+++ server/spec/requests/users_spec.rb
@@ -2,9 +2,17 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   describe "POST /users" do
+    let(:invalid_params) do
+      {
+        name: "user name",
+        email: "invalid email",
+        password: "password"
+      }
+    end
+
     context "when params invalid" do
       it "should return 422 status code" do
-        post users_path, params: invalid_params
+        post api_users_path, params: invalid_params
 
         expect(response).to have_http_status :unprocessable_entity
       end
