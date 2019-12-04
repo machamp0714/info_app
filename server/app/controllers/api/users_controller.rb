66fa9d6f7520bb983b647f4c6ddfd47c67ea65a5
25 macamp <rateliasudaisuki@gmail.com>
2019-12-04 19:41:29 +0900

    users_controller createアクションを修正 #3

--- server/app/controllers/api/users_controller.rb
+++ server/app/controllers/api/users_controller.rb
@@ -5,13 +5,13 @@ class Api::UsersController < ApplicationController
     if user.save
       render json: user, status: :created
     else
-      render json: user.errors.messages, status: :unprocessable_entity
+      render json: { errors: user.errors }, status: :unprocessable_entity
     end
   end
 
   private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password)
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
 end
