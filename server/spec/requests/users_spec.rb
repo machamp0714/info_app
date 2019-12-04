6c4e3f224ea75167bff056fd260e7286a74278ef
25 macamp <rateliasudaisuki@gmail.com>
2019-12-04 19:43:11 +0900

    適当なerrors jsonが返ることをテスト #3

--- server/spec/requests/users_spec.rb
+++ server/spec/requests/users_spec.rb
@@ -4,17 +4,42 @@ RSpec.describe "Users", type: :request do
   describe "POST /users" do
     let(:invalid_params) do
       {
-        name: "user name",
-        email: "invalid email",
-        password: "password"
+        user: {
+          name: "",
+          email: "",
+          password: ""
+        }
       }
     end
 
     context "when params invalid" do
+      subject { post api_users_path, params: invalid_params }
+
       it "should return 422 status code" do
-        post api_users_path, params: invalid_params
+        subject
 
         expect(response).to have_http_status :unprocessable_entity
+      end
+
+      it "should return errors json" do
+        errors = {
+          "errors" => {
+            "password" => [
+              "can't be blank",
+              "is too short (minimum is 8 characters)"
+            ],
+            "name" => [
+              "can't be blank"
+            ],
+            "email" => [
+              "can't be blank",
+              "is invalid"
+            ]
+          }
+        }
+        subject
+
+        expect(json).to eq errors
       end
     end
   end
