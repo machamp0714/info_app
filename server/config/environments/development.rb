eeb5d19c3a0c7de2eedfd196ffd5b642bf928b9b
25 macamp <rateliasudaisuki@gmail.com>
2019-12-04 12:34:56 +0900

    better_errors&pry-byebugをインストール #1

--- server/config/environments/development.rb
+++ server/config/environments/development.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+BetterErrors::Middleware.allow_ip! '0.0.0.0/0'
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
