ed070c095ec3fd8e8c7eefc446f4db1bd7a0de2a
25 macamp <rateliasudaisuki@gmail.com>
2019-12-07 13:38:04 +0900

    ログイン成功時のテストを追加 #3

--- server/app/controllers/api/auth/sessions_controller.rb
+++ server/app/controllers/api/auth/sessions_controller.rb
@@ -1,2 +1,17 @@
-class Api::Auth::SessionsController < ApplicationController
+class Api::Auth::SessionsController < DeviseTokenAuth::SessionsController
+  private
+
+  def resource_params
+    params.permit(:email, :password)
+  end
+
+  protected
+
+  def render_create_success
+    render json: @resource, status: :ok
+  end
+
+  def render_create_error_bad_credentials
+    render json: { status: 401, message: "メールアドレスかパスワードが間違っています。" }, status: :unauthorized
+  end
 end
--- server/spec/requests/sessions_spec.rb
+++ server/spec/requests/sessions_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "Sessions", type: :request do
-  describe "POST /login" do
+  describe "POST /api/auth/sign_in" do
     let(:user) { create :user }
 
     context "when invalid params" do
-      let(:invalid_params) { { session: { email: "", password: "" } } }
-      subject(:login_path) { post api_user_sessions_path, params: invalid_params }
+      let(:invalid_params) { { email: "", password: "" } }
+      subject(:login_path) { post api_user_session_path, params: invalid_params }
 
       it "should return 401 status code" do
         login_path
@@ -19,19 +19,25 @@ RSpec.describe "Sessions", type: :request do
 
         expect(json).to include({
           "status" => 401,
-          "title" => "メールアドレスかパスワードが間違っています。"
+          "message" => "メールアドレスかパスワードが間違っています。"
         })
       end
     end
 
     context "when valid params" do
-      let(:valid_params) { { session: { email: user.email, password: user.password } } }
+      let(:valid_params) { { email: user.email, password: user.password } }
       subject(:login_path) { post api_user_session_path, params: valid_params }
 
-      it "should return 201 status code" do
+      it "should return 200 status code" do
         login_path
 
-        expect(response).to have_http_status :created
+        expect(response).to have_http_status :ok
+      end
+
+      it "gives you to access_token if login success" do
+        login_path
+
+        expect(response.has_header?("access-token")).to be_truthy
       end
     end
   end
