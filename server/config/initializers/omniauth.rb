4ebc4083d07b72fcae0c6af70e1e928e84ab9008
25 macamp <rateliasudaisuki@gmail.com>
2019-12-08 16:09:09 +0900

    omniauth-twitterをインストール #6

--- server/config/initializers/omniauth.rb
+++ server/config/initializers/omniauth.rb
@@ -2,4 +2,5 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, ENV["GITHUB_KEY"], ENV["GITHUB_SECRET"], scope: "user"
+  provider :twitter, ENV["TWITTER_KEY"], ENV["TWITTER_SECRET"]
 end
