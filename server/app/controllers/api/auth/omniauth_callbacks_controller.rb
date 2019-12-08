85318c28d24b0919c38cbc62bbf1f8dbbcb45649
25 macamp <rateliasudaisuki@gmail.com>
2019-12-08 16:10:28 +0900

    headersを更新するタイミングを修正 #6

--- server/app/controllers/api/auth/omniauth_callbacks_controller.rb
+++ server/app/controllers/api/auth/omniauth_callbacks_controller.rb
@@ -1,18 +1,31 @@
 class Api::Auth::OmniauthCallbacksController < DeviseTokenAuth::OmniauthCallbacksController
+  def redirect_callbacks
+    # derive target redirect route from "resource_class" param, which was set
+    # before authentication.
+    devise_mapping = get_devise_mapping
+    redirect_route = get_redirect_route(devise_mapping)
+
+    # preserve omniauth info for success route. ignore "extra" in twitter
+    # auth response to avoid CookieOverflow.
+    session["dta.omniauth.auth"] = request.env["omniauth.auth"].except("extra")
+    session["dta.omniauth.params"] = request.env["omniauth.params"]
+
+    redirect_to redirect_route
+  end
+
   def omniauth_success
     get_resource_from_auth_hash
     set_token_on_resource
     create_auth_params
-    update_auth_header
 
     sign_in(:user, @resource, store: false, bypass: false)
 
-    @resource.save!
+    if @resource.save
+      update_auth_header
 
-    render json: @resource, status: :ok
-  end
-
-  def omniauth_failure
-    render json: { status: 401, message: "認証に失敗しました" }, status: :unauthorized
+      render json: @resource, status: :ok
+    else
+      render json: { status: 401, message: "認証に失敗しました" }, status: :unauthorized
+    end
   end
 end
