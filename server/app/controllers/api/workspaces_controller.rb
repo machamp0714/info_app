6b98f66625f3d53491e1b84343a3bed1bba92039
25 macamp <rateliasudaisuki@gmail.com>
2019-12-09 23:49:54 +0900

    workspaces controller verify_permissionメソッドを追加 #8
    権限を検証するため

--- server/app/controllers/api/workspaces_controller.rb
+++ server/app/controllers/api/workspaces_controller.rb
@@ -1,5 +1,6 @@
 class Api::WorkspacesController < ApplicationController
   before_action :authenticate_api_user!
+  before_action :verify_permission, only: %i[update]
 
   def create
     workspace = current_api_user.workspaces.build(workspace_params)
@@ -10,9 +11,18 @@ class Api::WorkspacesController < ApplicationController
     end
   end
 
+  def update; end
+
   private
 
   def workspace_params
     params.permit(:name)
+  end
+
+  def verify_permission
+    workspace = Workspace.find(params[:id])
+    return if workspace.user_id == current_api_user.id
+
+    render json: { status: 403, message: "権限がありません" }, status: :forbidden
   end
 end
