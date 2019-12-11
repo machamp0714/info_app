296e3ce8f7224dcad2fae15b12432516d43daac6
25 macamp <rateliasudaisuki@gmail.com>
2019-12-11 20:29:02 +0900

    tasks controllerへcreateアクションを追加 #12

--- server/app/controllers/api/tasks_controller.rb
+++ server/app/controllers/api/tasks_controller.rb
@@ -2,9 +2,23 @@ class Api::TasksController < ApplicationController
   before_action :authenticate_api_user!
   before_action :verify_permission
 
-  def create; end
+  def create
+    workspace = current_api_user.workspaces.find(params[:workspace_id])
+    column = workspace.columns.find(params[:column_id])
+    task = column.tasks.build(task_params.merge(user_id: current_api_user.id))
+
+    if task.save
+      render json: task, status: :created
+    else
+      render_errors(task)
+    end
+  end
 
   private
+
+  def task_params
+    params.permit(:title, :description)
+  end
 
   def verify_permission
     workspace = Workspace.find(params[:workspace_id])
