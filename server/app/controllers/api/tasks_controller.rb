992a1a4b15edb2e866f66b5437d09230e48dd1fd
25 macamp <rateliasudaisuki@gmail.com>
2019-12-12 00:21:16 +0900

    tasks controllerのcreateアクションを修正 #12

--- server/app/controllers/api/tasks_controller.rb
+++ server/app/controllers/api/tasks_controller.rb
@@ -3,8 +3,7 @@ class Api::TasksController < ApplicationController
   before_action :verify_permission
 
   def create
-    workspace = current_api_user.workspaces.find(params[:workspace_id])
-    column = workspace.columns.find(params[:column_id])
+    column = Column.find(params[:column_id])
     task = column.tasks.build(task_params.merge(user_id: current_api_user.id))
 
     if task.save
@@ -23,8 +22,8 @@ class Api::TasksController < ApplicationController
   end
 
   def verify_permission
-    workspace = Workspace.find(params[:workspace_id])
-    return if workspace.user_id == current_api_user.id
+    column = Column.find(params[:column_id])
+    return if column.user_id == current_api_user.id
 
     render_permission_error
   end
