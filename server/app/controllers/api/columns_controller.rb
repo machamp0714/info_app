876d055726ad0d2a7a3afbd8bcd5838ffae97f8e
25 macamp <rateliasudaisuki@gmail.com>
2019-12-11 23:18:21 +0900

    columns controllerにverify_columnメソッドを追加 #12

--- server/app/controllers/api/columns_controller.rb
+++ server/app/controllers/api/columns_controller.rb
@@ -2,7 +2,8 @@
 
 class Api::ColumnsController < ApplicationController
   before_action :authenticate_api_user!
-  before_action :verify_permission
+  before_action :verify_permission, only: %i[create]
+  before_action :verify_column, only: %i[update]
 
   def create
     workspace = Workspace.find(params[:workspace_id])
@@ -42,6 +43,13 @@ class Api::ColumnsController < ApplicationController
   def verify_permission
     workspace = Workspace.find(params[:workspace_id])
     return if workspace.user_id == current_api_user.id
+
+    render_permission_error
+  end
+
+  def verify_column
+    column = Column.find(params[:id])
+    return if column.user_id == current_api_user.id
 
     render_permission_error
   end
