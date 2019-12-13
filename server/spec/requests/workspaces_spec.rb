8a2e5ad4a5f57874339a5e026be8ca2d8f731e10
25 macamp <rateliasudaisuki@gmail.com>
2019-12-13 22:54:02 +0900

    rubocopでコードの修正 #12

--- server/spec/requests/workspaces_spec.rb
+++ server/spec/requests/workspaces_spec.rb
@@ -15,8 +15,6 @@ RSpec.describe "Workspaces", type: :request do
 
       let(:user) { create :user }
       let(:auth_headers) { user.create_new_auth_token }
-      let!(:workspace) { create :workspace, user: user }
-      let!(:other_workspace) { create :workspace, user: user }
 
       it "return 200 status code" do
         get_success
