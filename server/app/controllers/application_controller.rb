ccacb64d116227d339a99b09a34fb7d5e3a28142
25 macamp <rateliasudaisuki@gmail.com>
2019-12-25 19:25:32 +0900

    csrf_tokenをcookieに格納するメソッドを追加 #18

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -2,10 +2,17 @@
 
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+  include ActionController::Cookies
+  include ActionController::RequestForgeryProtection
 
+  before_action :set_csrf_token
   rescue_from ActiveRecord::RecordNotFound, with: :not_found_error
 
   private
+
+  def set_csrf_token
+    cookies["CSRF_TOKEN"] = form_authenticity_token
+  end
 
   def render_errors(model, data = nil)
     response = {
