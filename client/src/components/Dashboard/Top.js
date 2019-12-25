358b08a7c4015f72f083174e7a9b1c756790f869
25 macamp <rateliasudaisuki@gmail.com>
2019-12-25 09:29:46 +0900

    TopComponentにheaderを追加 #18

--- client/src/components/Dashboard/Top.js
+++ client/src/components/Dashboard/Top.js
@@ -1,7 +1,19 @@
 import React from "react";
+import "../../styles/Dashboard/navbar.scss";
 
 const Top = () => {
-  return <div>Hello, React</div>;
+  return (
+    <div>
+      <div className="header">
+        <div className="header-content">
+          <ul className="header-menu">
+            <li className="header-item">Sign up</li>
+            <li className="header-item">Sign in</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Top;
