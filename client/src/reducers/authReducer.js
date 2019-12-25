5de5137c0c44304308215a788a4f73b2253af910
25 macamp <rateliasudaisuki@gmail.com>
2019-12-25 23:49:37 +0900

    authReducerを作成 #18

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,5 +1,26 @@
-const initState = {};
+const initState = {
+  loggedIn: false
+};
 
-const authReducer = (state = initState, action) => {};
+const authReducer = (state = initState, action) => {
+  switch (action.type) {
+    case "SIGNUP_SUCCESS":
+      console.log(action.user);
+      return {
+        ...state,
+        user: action.user,
+        loggedIn: true
+      };
+    case "SIGNUP_ERROR":
+      console.log(action.error);
+      return {
+        ...state,
+        error: action.error,
+        loggedIn: false
+      };
+    default:
+      return state;
+  }
+};
 
 export default authReducer;
