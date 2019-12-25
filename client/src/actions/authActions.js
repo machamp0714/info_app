274b57d86a1c9510a1125f8f14a014c5c1e1d390
25 macamp <rateliasudaisuki@gmail.com>
2019-12-26 00:03:37 +0900

    authReducerを修正 #18
    signupの出力を修正

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -5,7 +5,7 @@ export const signup = values => {
     request
       .post("http://localhost:3001/api/auth", values)
       .then(response => {
-        dispatch({ type: "SIGNUP_SUCCESS", user: response });
+        dispatch({ type: "SIGNUP_SUCCESS", payload: response.data });
       })
       .catch(error => {
         dispatch({ type: "SIGNUP_ERROR", error: error });
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -5,17 +5,12 @@ const initState = {
 const authReducer = (state = initState, action) => {
   switch (action.type) {
     case "SIGNUP_SUCCESS":
-      console.log(action.user);
       return {
-        ...state,
-        user: action.user,
+        data: action.payload,
         loggedIn: true
       };
     case "SIGNUP_ERROR":
-      console.log(action.error);
       return {
-        ...state,
-        error: action.error,
         loggedIn: false
       };
     default:
