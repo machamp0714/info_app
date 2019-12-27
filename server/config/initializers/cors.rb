06a0a9080904b07537a0377845e5022e737d71be
25 macamp <rateliasudaisuki@gmail.com>
2019-12-27 19:50:06 +0900

    rubocopでコードを修正 #18

--- server/config/initializers/cors.rb
+++ server/config/initializers/cors.rb
@@ -6,9 +6,9 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins "localhost:3000"
 
     resource "*",
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true,
-      expose: %w[access-token client uid]
+             headers: :any,
+             methods: %i[get post put patch delete options head],
+             credentials: true,
+             expose: %w[access-token client uid]
   end
 end
