d4148988b9d4fa08e6419ece54d005ad8cf6a803
25 macamp <rateliasudaisuki@gmail.com>
2019-12-27 22:24:09 +0900

    no-useless-concatを修正 #18
    circleciでbuildに失敗したため

--- client/src/components/Button/IconButton.js
+++ client/src/components/Button/IconButton.js
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const IconButton = ({ icon }) => {
   return (
-    <button className={"sns-icon-button" + " " + icon}>
+    <button className={"sns-icon-button " + icon}>
       <FontAwesomeIcon icon={["fab", icon]} />
       <span className="button-text">{icon}</span>
     </button>
