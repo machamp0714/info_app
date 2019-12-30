8ce45874b806d7458492dd1992e35d112e925244
25 macamp <rateliasudaisuki@gmail.com>
2019-12-30 14:04:15 +0900

    IconButtonにonClickイベントを追加 #24

--- client/src/components/Button/IconButton.js
+++ client/src/components/Button/IconButton.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const IconButton = ({ icon }) => {
+const IconButton = ({ icon, handleClick }) => {
   return (
-    <button className={"sns-icon-button " + icon}>
+    <button onClick={handleClick} className={"sns-icon-button " + icon}>
       <FontAwesomeIcon icon={["fab", icon]} />
       <span className="button-text">{icon}</span>
     </button>
