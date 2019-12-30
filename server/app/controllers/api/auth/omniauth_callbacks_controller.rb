d18dc9eea698586f56c77060882e4929ec18ae8e
25 macamp <rateliasudaisuki@gmail.com>
2019-12-30 14:04:54 +0900

    oauth認証に成功した場合の処理を追加 #24

--- server/app/controllers/api/auth/omniauth_callbacks_controller.rb
+++ server/app/controllers/api/auth/omniauth_callbacks_controller.rb
@@ -1,33 +1,62 @@
 # frozen_string_literal: true
 
+require "net/https"
+
 class Api::Auth::OmniauthCallbacksController < DeviseTokenAuth::OmniauthCallbacksController
+  def twitter
+    oauth_url = "https://github.com/login/oauth/authorize?client_id=#{ENV['GITHUB_KEY']}&scope=read%20user"
+
+    render json: { url: oauth_url }, status: :ok
+  end
+
   def redirect_callbacks
-    # derive target redirect route from "resource_class" param, which was set
-    # before authentication.
-    devise_mapping = get_devise_mapping
-    redirect_route = get_redirect_route(devise_mapping)
+    uri = URI.parse("https://github.com/login/oauth/access_token")
+    https = Net::HTTP.new(uri.host, uri.port)
+    https.use_ssl = true
+    request = Net::HTTP::Post.new(uri.path)
+    data = {
+      client_id: ENV["GITHUB_KEY"],
+      client_secret: ENV["GITHUB_SECRET"],
+      code: params[:code]
+    }
+    request.body = data.to_json
+    request["Content-Type"] = "application/json"
+    request["Accept"] = "application/json"
 
-    # preserve omniauth info for success route. ignore "extra" in twitter
-    # auth response to avoid CookieOverflow.
-    session["dta.omniauth.auth"] = request.env["omniauth.auth"].except("extra")
-    session["dta.omniauth.params"] = request.env["omniauth.params"]
+    response = https.start { |http| http.request(request) }
 
-    redirect_to redirect_route
+    access_token = JSON.parse(response.body).fetch("access_token")
+    cookies[:access_token] = access_token
+
+    redirect_to "http://localhost:3001/api/auth/github/callback"
   end
 
   def omniauth_success
-    get_resource_from_auth_hash
-    set_token_on_resource
-    create_auth_params
+    uri = URI.parse("https://api.github.com/user")
+    https = Net::HTTP.new(uri.host, uri.port)
+    https.use_ssl = true
 
-    sign_in(:user, @resource, store: false, bypass: false)
+    request = Net::HTTP::Get.new(uri.path)
+    request["Authorization"] = "token #{cookies[:access_token]}"
 
-    if @resource.save
-      update_auth_header
+    res = https.start { |http| http.request(request) }
 
-      render json: @resource, status: :ok
-    else
-      render_errors(@resource)
-    end
+    auth = JSON.parse(res.body)
+
+    user = User.find_by(uid: auth["id"], provider: "github")
+    user ||= User.create(
+      uid: auth["id"],
+      name: auth["login"],
+      image: auth["avatar_url"],
+      provider: "github",
+      email: User.get_email(auth),
+      password: Devise.friendly_token[0, 20]
+    )
+    token = user.create_token
+    header = user.build_auth_header(token.token, token.client)
+    cookies["access-token"] = header["access-token"]
+    cookies["client"] = header["client"]
+    cookies["uid"] = header["uid"]
+    cookies["expiry"] = header["expiry"]
   end
 end
