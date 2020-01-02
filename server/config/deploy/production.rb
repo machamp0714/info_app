45b9e20721655ebd1b420dd342ae2bbeee0b8236
25 macamp <rateliasudaisuki@gmail.com>
2020-01-02 14:34:15 +0900

    cap本番環境の設定を追加 #15

--- server/config/deploy/production.rb
+++ server/config/deploy/production.rb
@@ -3,7 +3,9 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-# server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
+server "0.0.0.0", user: "tatsuya", roles: %w[app db web]
+
+set :ssh_options, keys: "~/.ssh/amazon-linux2"
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
