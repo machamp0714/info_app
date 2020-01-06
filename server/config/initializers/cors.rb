34e7895907bb506036b4a270e09fae3a67332923
25 macamp <rateliasudaisuki@gmail.com>
2020-01-06 10:18:00 +0900

    corsの設定を修正 #15

--- server/config/initializers/cors.rb
+++ server/config/initializers/cors.rb
@@ -3,7 +3,7 @@
 Rails.application.config.action_controller.forgery_protection_origin_check = false
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:3000"
+    origins "localhost:3000", "info-app.com.s3-website-ap-northeast-1.amazonaws.com"
 
     resource "*",
              headers: :any,
