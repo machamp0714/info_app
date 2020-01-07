ae7ff224b6c99f09dfdf275fe0bace08d97517b4
25 macamp <rateliasudaisuki@gmail.com>
2020-01-07 13:32:07 +0900

    AppComponentのuseEffectを修正 #25

--- client/src/App.js
+++ client/src/App.js
@@ -10,13 +10,13 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { far } from "@fortawesome/free-regular-svg-icons";
-import axios from "axios";
+import { client } from "./config/axios";
 
 library.add(fab, fas, far);
 
 const App = () => {
   useEffect(() => {
-    axios.get("http://localhost:3001/csrf_token", { withCredentials: true });
+    client.get("/csrf_token", { withCredentials: true });
   });
 
   return (
