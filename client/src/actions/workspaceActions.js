b11b1a6d96ff3749e5a4edd15b04d4c09f55de16
25 macamp <rateliasudaisuki@gmail.com>
2020-01-08 22:13:28 +0900

    workspace一覧を取得するActionを追加 #25

--- client/src/actions/workspaceActions.js
+++ client/src/actions/workspaceActions.js
@@ -1,4 +1,14 @@
-import { request } from "../config/axios";
+import { request, client } from "../config/axios";
+
+const getSuccess = response => ({
+  type: "GET_SUCCESS",
+  payload: response.data
+});
+
+const getError = error => ({
+  type: "GET_ERROR",
+  payload: error
+});
 
 const createSuccess = response => ({
   type: "CREATE_SUCCESS",
@@ -16,5 +26,14 @@ export const createWorkspace = param => {
       .post("/api/workspaces", param)
       .then(response => dispatch(createSuccess(response)))
       .catch(error => dispatch(createError(error)));
+  };
+};
+
+export const getWorkspaces = () => {
+  return dispatch => {
+    client
+      .get("/api/workspaces")
+      .then(response => dispatch(getSuccess(response)))
+      .catch(error => dispatch(getError(error)));
   };
 };
