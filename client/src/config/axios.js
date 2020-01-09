8e0a71e7cf95d7effbb16c838576aa7e69e6d9c1
25 macamp <rateliasudaisuki@gmail.com>
2020-01-09 12:02:54 +0900

    axiosのデフォルトにヘッダー情報を追加 #25

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -4,9 +4,19 @@ export const request = axios.create({
   xsrfCookieName: "CSRF_TOKEN",
   xsrfHeaderName: "X-CSRF-Token",
   withCredentials: true,
-  baseURL: process.env.REACT_APP_API_HOST
+  baseURL: process.env.REACT_APP_API_HOST,
+  headers: {
+    "access-token": localStorage.getItem("access-token") || null,
+    client: localStorage.getItem("client") || null,
+    uid: localStorage.getItem("uid") || null
+  }
 });
 
 export const client = axios.create({
-  baseURL: process.env.REACT_APP_API_HOST
+  baseURL: process.env.REACT_APP_API_HOST,
+  headers: {
+    "access-token": localStorage.getItem("access-token") || null,
+    client: localStorage.getItem("client") || null,
+    uid: localStorage.getItem("uid") || null
+  }
 });
