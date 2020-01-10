ba5bf2024263eba7568ab69996ee0408d5dc9c16
25 macamp <rateliasudaisuki@gmail.com>
2020-01-10 14:28:06 +0900

    AddWorkspaceComponentを作成 #32

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import SignedinNavbar from "../Layout/SignedinNavbar";
 import Sidebar from "./Sidebar";
-import CreateWorkspace from "../../containers/Workspace/CreateWorkspace";
+import AddWorkspace from "./AddWorkspace";
 
 const Dashboard = ({ workspaces, isLoading, getWorkspaces }) => {
   const [open, setOpen] = useState(false);
@@ -29,13 +29,7 @@ const Dashboard = ({ workspaces, isLoading, getWorkspaces }) => {
         workspaces={workspaces}
       />
       <Sidebar open={open} handleDrawerClose={handleDrawerClose} />
-      <div className="none-workspace">
-        <div className="workspace-description">
-          <p>まだworkspaceがありません。</p>
-          <p>まずはworkspaceを作成しましょう！</p>
-        </div>
-        <CreateWorkspace />
-      </div>
+      <AddWorkspace />
     </div>
   );
 };
