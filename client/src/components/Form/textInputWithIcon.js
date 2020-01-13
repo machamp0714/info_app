0d9614ea9a8a178baede9cc9cd686373ebf0f9b8
25 macamp <rateliasudaisuki@gmail.com>
2020-01-13 15:05:02 +0900

    AuthFormを修正 #35

--- client/src/components/Form/textInputWithIcon.js
+++ client/src/components/Form/textInputWithIcon.js
@@ -1,20 +1,6 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import IconButton from "@material-ui/core/IconButton";
 import InputBase from "@material-ui/core/InputBase";
 import clsx from "clsx";
-
-const useStyles = makeStyles(theme => ({
-  button: {
-    "&:hover": {
-      backgroundColor: "#FFFFFF",
-      cursor: "default"
-    },
-    "&:focus": {
-      backgroundColor: "#FFFFFF"
-    }
-  }
-}));
 
 const TextInputWithIcon = ({
   icon,
@@ -24,11 +10,9 @@ const TextInputWithIcon = ({
   handleChange,
   error
 }) => {
-  const classes = useStyles();
-
   return (
     <div className={clsx("paper-box", { "mb-4": !error })}>
-      <IconButton className={classes.button}>{icon}</IconButton>
+      <div className="icon-box">{icon}</div>
       <InputBase
         id={id}
         type={type}
