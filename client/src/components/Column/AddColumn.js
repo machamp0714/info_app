b5a91249eab36c32b4170abe1c24ad7798c9991e
25 macamp <rateliasudaisuki@gmail.com>
2020-01-19 22:34:14 +0900

    AddColumnButtonを作成 #37

--- client/src/components/Column/AddColumn.js
+++ client/src/components/Column/AddColumn.js
@@ -1,11 +1,32 @@
 import React from "react";
+import { makeStyles } from "@material-ui/styles";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles(theme => ({
+  button: {
+    width: 125,
+    height: 40,
+    backgroundColor: "#11CDEF",
+    color: "#FFFFFF",
+    boxShadow: "none",
+    "&:hover": {
+      backgroundColor: "#11CDEF",
+      boxShadow: "none"
+    }
+  }
+}));
 
 const AddColumn = () => {
+  const classes = useStyles();
+
   return (
     <div className="center-content">
-      <div className="workspace-description">
-        このワークスペースにはカラムがありません。
+      <div className="column-description">
+        <p>このワークスペースにはカラムがありません。</p>
       </div>
+      <Button className={classes.button} variant="contained">
+        Add Column
+      </Button>
     </div>
   );
 };
