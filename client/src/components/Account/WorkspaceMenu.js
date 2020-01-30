dbdd14470f061fbd4b4765002a7279d591c0ed58
25 macamp <rateliasudaisuki@gmail.com>
2020-01-30 23:50:47 +0900

    workspaceの編集機能を実装 #42

--- client/src/components/Account/WorkspaceMenu.js
+++ client/src/components/Account/WorkspaceMenu.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import EditIcon from "@material-ui/icons/Edit";
 import DeleteIcon from "@material-ui/icons/Delete";
+import StandardModal from "../Shared/StandardModal";
 
 const iconStyle = {
   color: "#4E545A",
@@ -12,16 +13,56 @@ const iconStyle = {
   }
 };
 
-const WorkspaceMenu = ({ workspace, deleteWorkspace }) => {
+const WorkspaceMenu = ({ workspace, editWorkspace, deleteWorkspace }) => {
+  const [name, setName] = useState(workspace.name);
+  const [open, setOpen] = useState(false);
+
   const handleDeleteClick = workspace_id => {
     deleteWorkspace(workspace_id);
   };
 
+  const handleOpenModal = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleChange = e => {
+    setName(e.target.value);
+  };
+
+  const handleClick = () => {
+    editWorkspace(workspace.id, { name: name });
+    setOpen(false);
+  };
+
+  const handleSubmit = e => {
+    editWorkspace(workspace.id, { name: name });
+    setOpen(false);
+    e.preventDefault();
+  };
+
   return (
     <div className="workspace-meta">
-      <button className="button-none column-menu-button" type="button">
+      <button
+        onClick={handleOpenModal}
+        className="button-none column-menu-button"
+        type="button"
+      >
         <EditIcon style={iconStyle} />
       </button>
+      <StandardModal
+        name={name}
+        open={open}
+        title="Edit Workspace"
+        placeholder="Enter workspace name"
+        handleClose={handleClose}
+        handleChange={handleChange}
+        handleClick={handleClick}
+        handleSubmit={handleSubmit}
+      />
       <button
         className="button-none column-menu-button"
         type="button"
--- client/src/containers/Account/WorkspaceMenu.js
+++ client/src/containers/Account/WorkspaceMenu.js
@@ -1,8 +1,10 @@
-import { deleteWorkspace } from "../../actions/workspaceActions";
+import { editWorkspace, deleteWorkspace } from "../../actions/workspaceActions";
 import WorkspaceMenu from "../../components/Account/WorkspaceMenu";
 import { connect } from "react-redux";
 
 const mapDispatchToProps = dispatch => ({
+  editWorkspace: (workspace_id, params) =>
+    dispatch(editWorkspace(workspace_id, params)),
   deleteWorkspace: workspace_id => dispatch(deleteWorkspace(workspace_id))
 });
 
