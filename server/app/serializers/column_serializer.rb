b8045f0073eafa9edfe9fa42cc6eb3c5f21797a7
25 macamp <rateliasudaisuki@gmail.com>
2020-01-31 12:08:11 +0900

    columun_serializerを修正 #42

--- server/app/serializers/column_serializer.rb
+++ server/app/serializers/column_serializer.rb
@@ -4,4 +4,8 @@ class ColumnSerializer < ActiveModel::Serializer
   attributes :id, :name
 
   belongs_to :workspace
+
+  class WorkspaceSerializer < ActiveModel::Serializer
+    attributes :id, :name
+  end
 end
