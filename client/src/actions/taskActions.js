cffe33db99892d3fd5a0deca993a16db9e1eafc6
25 macamp <rateliasudaisuki@gmail.com>
2020-01-31 16:41:07 +0900

    createTaskアクションを追加 #44

--- client/src/actions/taskActions.js
+++ client/src/actions/taskActions.js
@@ -1,4 +1,5 @@
 import { http, secureHTTP } from "../config/axios";
+import { createColumn } from "./columnAction";
 
 const getSuccess = response => ({
   type: "GET_TASKS_SUCCESS",
@@ -10,11 +11,30 @@ const getError = error => ({
   payload: error.response.data
 });
 
+const createSuccess = response => ({
+  type: "CREATE_TASK_SUCCESS",
+  payload: response.data
+});
+
+const createError = error => ({
+  type: "CREATE_TASK_ERROR",
+  payload: error.response.data
+});
+
 export const getTasks = column_id => {
   return dispatch => {
     http
       .get(`/api/columns/${column_id}/tasks`)
       .then(response => dispatch(getSuccess(response)))
       .catch(error => dispatch(getError(error)));
+  };
+};
+
+export const createTask = (column_id, params) => {
+  return dispatch => {
+    secureHTTP
+      .post(`/api/columns/${column_id}/tasks`, params)
+      .then(response => dispatch(createSuccess(response)))
+      .catch(error => dispatch(createError(error)));
   };
 };
