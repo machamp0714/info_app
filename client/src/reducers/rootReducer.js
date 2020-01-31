16b2c603bdfee1ecba0c055cdeafde248cd211a6
25 macamp <rateliasudaisuki@gmail.com>
2020-01-31 16:45:47 +0900

    rootReducerにtaskReducerを追加 #44

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 import workspaceReducer from "./workspaceReducer";
 import columnReducer from "./columnReducer";
+import taskReducer from "./taskReducer";
 
 const rootReducer = combineReducers({
   auth: authReducer,
   workspace: workspaceReducer,
-  column: columnReducer
+  column: columnReducer,
+  task: taskReducer
 });
 
 export default rootReducer;
