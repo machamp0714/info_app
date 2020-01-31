d235259cade7463faa968d9e5d121b51034f9ec4
25 macamp <rateliasudaisuki@gmail.com>
2020-01-31 17:35:04 +0900

    ColumnDetailComponentにAddTaskComponentを追加 #44

--- client/src/components/Column/ColumnDetails.js
+++ client/src/components/Column/ColumnDetails.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import AddIcon from "@material-ui/icons/Add";
+import AddTask from "../../containers/Task/AddTask";
 import ColumnMenu from "../../containers/Column/ColumnMenu";
 
 const iconStyle = {
@@ -12,18 +13,29 @@ const iconStyle = {
   }
 };
 
-const ColumnDetails = ({ column }) => {
+const ColumnDetails = ({ column, tasks }) => {
+  const [open, setOpen] = useState(false);
+
+  const handleToggle = () => {
+    setOpen(prevOpen => !prevOpen);
+  };
+
   return (
     <li className="column-box">
       <div className="column-header">
         <div className="column-name">{column.name}</div>
         <div className="column-menu">
-          <button type="button" className="column-menu-button button-none">
+          <button
+            onClick={handleToggle}
+            type="button"
+            className="column-menu-button button-none"
+          >
             <AddIcon style={iconStyle} />
           </button>
           <ColumnMenu column={column} />
         </div>
       </div>
+      {open && <AddTask handleToggle={handleToggle} />}
     </li>
   );
 };
--- client/src/containers/Column/ColumnDetails.js
+++ client/src/containers/Column/ColumnDetails.js
@@ -2,9 +2,13 @@ import ColumnDetails from "../../components/Column/ColumnDetails";
 import { editColumn, deleteColumn } from "../../actions/columnAction";
 import { connect } from "react-redux";
 
+const mapStateToProps = state => ({
+  tasks: state.task.tasks
+});
+
 const mapDispatchToProps = dispatch => ({
   deleteColumn: column_id => dispatch(deleteColumn(column_id)),
   editColumn: (column_id, params) => dispatch(editColumn(column_id, params))
 });
 
-export default connect(null, mapDispatchToProps)(ColumnDetails);
+export default connect(mapStateToProps, mapDispatchToProps)(ColumnDetails);
