0cc295defb7e023743f94dd236f6e27b1ec23128
25 macamp <rateliasudaisuki@gmail.com>
2020-02-04 15:00:33 +0900

    taskにcreated_atを表示 #48

--- client/src/components/Task/TaskDetails.js
+++ client/src/components/Task/TaskDetails.js
@@ -2,6 +2,27 @@ import React from "react";
 import TaskMenu from "../../containers/Task/TaskMenu";
 
 const TaskDetails = ({ task }) => {
+  const diffCreatedAt = () => {
+    const taskCreatedAt = new Date(task.created_at);
+    const now = new Date();
+    const monthDiff = now.getMonth() - taskCreatedAt.getMonth();
+    const dateDiff = now.getDate() - taskCreatedAt.getDate();
+
+    if (monthDiff === 0) {
+      if (dateDiff === 0) {
+        return "today";
+      } else if (dateDiff === 1) {
+        return "1 day ago";
+      } else {
+        return `${dateDiff} days ago`;
+      }
+    } else if (monthDiff === 1) {
+      return "1 month ago";
+    } else {
+      return `${monthDiff} months ago`;
+    }
+  };
+
   return (
     <article className="task-card">
       <div className="d-flex flex-row">
@@ -12,10 +33,7 @@ const TaskDetails = ({ task }) => {
               <div className="task-content">
                 <p className="mb-1">{task.content}</p>
               </div>
-              <small className="text-gray">
-                Added by
-                <span>2 days ago</span>
-              </small>
+              <small className="text-gray">{diffCreatedAt()}</small>
             </div>
           </div>
         </div>
