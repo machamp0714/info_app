667b5b18b1dae0f0eafd22d20ee8d8bc6acce70f
25 macamp <rateliasudaisuki@gmail.com>
2020-02-11 20:53:47 +0900

    tasks_controllerのcreateアクションを修正 #56

--- server/app/controllers/api/tasks_controller.rb
+++ server/app/controllers/api/tasks_controller.rb
@@ -15,7 +15,13 @@ class Api::TasksController < ApplicationController
 
   def create
     column = Column.find(params[:column_id])
-    task = column.tasks.build(task_params.merge(user_id: current_api_user.id))
+    tasks = column.tasks
+    task = tasks.build(task_params.merge(user_id: current_api_user.id))
+    task.rank = if tasks.exists?
+                  tasks.maximum(:rank) + 1
+                else
+                  1
+                end
 
     if task.save
       render json: task, status: :created
