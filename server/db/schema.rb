83a6889bd41549ff2750c6668103aeb3a5ead86f
25 macamp <rateliasudaisuki@gmail.com>
2020-02-21 15:06:38 +0900

    AccessTokenモデルを作成 #14

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_11_103910) do
+ActiveRecord::Schema.define(version: 2020_02_21_060334) do
+
+  create_table "access_tokens", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "token", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_access_tokens_on_user_id"
+  end
 
   create_table "columns", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -56,6 +64,7 @@ ActiveRecord::Schema.define(version: 2020_02_11_103910) do
     t.index ["user_id"], name: "index_workspaces_on_user_id"
   end
 
+  add_foreign_key "access_tokens", "users"
   add_foreign_key "columns", "users"
   add_foreign_key "columns", "workspaces"
   add_foreign_key "tasks", "columns"
