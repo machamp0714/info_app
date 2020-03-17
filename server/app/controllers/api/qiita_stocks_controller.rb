d9b0b6ee7d43375ff350aed2d432265368c1cd3e
25 macamp <rateliasudaisuki@gmail.com>
2020-03-17 15:46:02 +0900

    qiitaアクションをリファクタリング #14

--- server/app/controllers/api/qiita_stocks_controller.rb
+++ server/app/controllers/api/qiita_stocks_controller.rb
@@ -4,30 +4,21 @@ class Api::QiitaStocksController < ApplicationController
   def qiita
     render_permission_error unless ENV["QIITA_STATE"] == params[:state]
 
-    uri = URI.parse("https://qiita.com/api/v2/access_tokens")
-    https = https_client(uri)
-
-    data = {
-      client_id: ENV["QIITA_KEY"],
-      client_secret: ENV["QIITA_SECRET"],
-      code: params[:code]
-    }
-
-    response = https.post(
-      uri.path,
-      data.to_json,
-      "content-type" => "application/json"
+    response = Faraday.post(
+      "https://qiita.com/api/v2/access_tokens",
+      data(params[:code]).to_s,
+      "Content-Type" => "application/json"
     )
     json = JSON.parse(response.body)
     token = json["token"]
 
-    authorize_uri = URI.parse("https://qiita.com/api/v2/authenticated_user")
-
-    https = https_client(authorize_uri)
-
-    response = https.get(authorize_uri.path, authorization: "Bearer #{token}")
-    result = JSON.parse(response.body)
-    user_id = result["id"]
+    conn = Faraday.new(
+      "https://qiita.com",
+      headers: { "Authorization" => "Bearer #{token}" }
+    )
+    response = conn.get("/api/v2/authenticated_user")
+    json = JSON.parse(response.body)
+    user_id = json["id"]
 
     qiita_stocks_form = QiitaStocksForm.new(user_id)
     job = qiita_stocks_form.async
@@ -35,7 +26,7 @@ class Api::QiitaStocksController < ApplicationController
     cookies[:job_id] = { value: job.id, expires: 1.day }
 
     user = User.find(redis.get(:user_id).to_i)
-    redirect_to "http://localhost:3000/#{user.name}/settings"
+    redirect_to "#{ENV['SERVER_HOST']}/#{user.name}/settings"
   end
 
   def check_async
@@ -47,6 +38,14 @@ class Api::QiitaStocksController < ApplicationController
   end
 
   private
+
+  def data(code)
+    {
+      client_id: ENV["QIITA_KEY"],
+      client_secret: ENV["QIITA_SECRET"],
+      code: code
+    }.to_json
+  end
 
   def asyncing?
     Delayed::Job.exists? params[:job_id]
