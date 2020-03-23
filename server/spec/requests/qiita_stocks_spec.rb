65d6f28a6057bafc1bcc2797f7defa2779306b94
25 macamp <rateliasudaisuki@gmail.com>
2020-03-24 00:08:10 +0900

    get api_qiita_stocks_pathのテストを追加 #74
    green

--- server/spec/requests/qiita_stocks_spec.rb
+++ server/spec/requests/qiita_stocks_spec.rb
@@ -9,5 +9,29 @@ RSpec.describe "QiitaStocks", type: :request do
 
       it_behaves_like "unauthorized_error"
     end
+
+    context "requests with authorized header" do
+      subject(:get_stocks) { get api_qiita_stocks_path, headers: auth_headers }
+
+      let(:user) { create :user }
+      let(:auth_headers) { user.create_new_auth_token }
+      let!(:stocks) { create_list :qiita_stock, 10, user: user }
+
+      it "returns 200 status code" do
+        get_stocks
+
+        expect(response).to have_http_status :ok
+      end
+
+      it "returns proper json body" do
+        get_stocks
+
+        expect(json).to include(
+          "id" => stocks[0]["id"],
+          "title" => stocks[0]["title"],
+          "url" => stocks[0]["url"]
+        )
+      end
+    end
   end
 end
